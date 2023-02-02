437db2d53d6cb47b952e49bad925f385d671abad
b <aldoruelasinzunza@gmail.com>
2023-02-02 16:54:54 -0600

    Change main.py to execute on lambda

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from PIL import Image
 import random
+import boto3
+
+s3 = boto3.cleint('s3')
+bucket = 'mys3bucketforwallpapers'
 
 def random_color():
     rgbl=[255,0,0]
@@ -12,8 +16,13 @@ def ChangePixel():
         for y in range(1080):
             pixels[x,y] = random_color()
 
-
+def lambda_handler(event, context):	
+	# delete old wallpaper
+    s3.delete_object(Bucket=bucket, Key='wallpaper.jpg')
+	# save new wallpaper
+    with open('/tmp/wallpaper.jpg', 'rb') as data:
+        s3.upload_fileobj(data, bucket, 'wallpaper.jpg')
 
 im= Image.new("RGB", (1920, 1080), "#FF0000")
 ChangePixel()
-im.show()
+im.save("/tmp/wallpaper.jpg")
